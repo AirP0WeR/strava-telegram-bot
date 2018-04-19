62b9d07525ae62d09ff3bd1ece2ecd4f470d27a8
pancham <panchamrb@phonepe.com>
2018-04-19 18:24:07 +0530

    1. Increase nonstop time to 15 minutes. 2. Add activity hyperlinks against stats. 3. Disable web page preview 4. Calculate biggest ride by parsing activities & remove biggest climb

--- bot.py
+++ bot.py
@@ -222,40 +222,62 @@ class FunStats(StravaApi, FormatValue):
                 if ('private' in activity) and (activity['private']):
                         stats['private'] += 1
 
-                if (activity['distance'] >= 70000.0) and ((activity['elapsed_time'] - activity['moving_time']) <= 120):
+                if (activity['distance'] >= 70000.0) and ((activity['elapsed_time'] - activity['moving_time']) <= 900):
                         stats['non_stop'] += 1
 
                 if activity['max_speed'] > stats['max_speed']:
                     stats['max_speed'] = activity['max_speed']
+                    stats['max_speed_activity'] = activity['id']
 
                 if activity['average_speed'] > stats['max_avg_speed']:
                     stats['max_avg_speed'] = activity['average_speed']
+                    stats['max_avg_speed_activity'] = activity['id']
 
                 if ('average_watts' in activity) and (activity['device_watts']):
                         if activity['average_watts'] > stats['average_watts']:
                             stats['average_watts'] = activity['average_watts']
+                            stats['average_watts_activity'] = activity['id']
                         if activity['max_watts'] > stats['max_watts']:
                             stats['max_watts'] = activity['max_watts']
+                            stats['max_watts_activity'] = activity['id']
 
                 if (activity['has_heartrate']) and (activity['max_heartrate'] > stats['max_heartrate']):
                         stats['max_heartrate'] = activity['max_heartrate']
+                        stats['max_heartrate_activity'] = activity['id']
 
                 if ('average_cadence' in activity) and (activity['average_cadence'] > stats['average_cadence']):
                         stats['average_cadence'] = activity['average_cadence']
+                        stats['average_cadence_activity'] = activity['id']
+
+                if activity['distance'] > stats['biggest_ride']:
+                    stats['biggest_ride'] = activity['distance']
+                    stats['biggest_ride_activity'] = activity['id']
+
+                if activity['total_elevation_gain'] > stats['max_elevation_gain']:
+                    stats['max_elevation_gain'] = activity['total_elevation_gain']
+                    stats['max_elevation_gain_activity'] = activity['id']
 
         return stats
 
     def get_stats(self):
         stats = {
             'biggest_ride': 0,
-            'biggest_climb': 0,
+            'biggest_ride_activity': '',
+            'max_elevation_gain': 0,
+            'max_elevation_gain_activity': '',
             'non_stop': 0,
             'max_avg_speed': 0.0,
+            'max_avg_speed_activity': '',
             'max_speed': 0.0,
+            'max_speed_activity': '',
             'average_watts': 0.0,
+            'average_watts_activity': '',
             'max_watts': 0,
+            'max_watts_activity': '',
             'max_heartrate': 0,
+            'max_heartrate_activity': '',
             'average_cadence': 0.0,
+            'average_cadence_activity': '',
             'achievement_count': 0,
             'following': 0,
             'followers': 0,
@@ -268,10 +290,7 @@ class FunStats(StravaApi, FormatValue):
         }
 
         athlete_info = self.get_athlete_info()
-        athlete_stats = self.get_athlete_stats(athlete_info['id'])
 
-        stats['biggest_ride'] = self.meters_to_kilometers(athlete_stats['biggest_ride_distance'])
-        stats['biggest_climb'] = self.remove_decimal_point(athlete_stats['biggest_climb_elevation_gain'])
         stats['following'] = athlete_info['friend_count']
         stats['followers'] = athlete_info['follower_count']
         stats['strava_created'] = self.date_to_human_readable(athlete_info['created_at'])
@@ -292,15 +311,15 @@ class FunStats(StravaApi, FormatValue):
         send_message(self.bot, self.update, greeting)
         stats = self.get_stats()
         message = "*Fun Stats:*\n\n" \
-                  "- _Max Power_: %s watts\n" \
-                  "- _Best Average Power_: %s watts\n" \
-                  "- _Max Speed_: %s kmph\n" \
-                  "- _Best Average Speed_: %s kmph\n" \
+                  "- _Max Power_: %s\n" \
+                  "- _Best Average Power_: %s\n" \
+                  "- _Max Speed_: %s\n" \
+                  "- _Best Average Speed_: %s\n" \
                   "- _Best Avg Cadence_: %s\n" \
-                  "- _Max Heartrate_: %s bpm\n" \
+                  "- _Max Heartrate_: %s\n" \
+                  "- _Biggest Ride_: %s\n" \
+                  "- _Max Elevation Gain_: %s\n" \
                   "- _Non-Stop Rides_: %s\n" \
-                  "- _Biggest Ride_: %s kms\n" \
-                  "- _Biggest Climb_: %s meters\n" \
                   "- _Total Break Time During Rides_: %s\n" \
                   "- _Using Strava Since_: %s\n" \
                   "- _Following Count_: %s\n" \
@@ -310,15 +329,15 @@ class FunStats(StravaApi, FormatValue):
                   "- _Private Rides_: %s\n" \
                   "- _Flagged Rides_: %s\n" \
                   "- _Bikes_: %s" % \
-                  (stats['max_watts'],
-                   stats['average_watts'],
-                   self.meters_per_second_to_kilometers(stats['max_speed']),
-                   self.meters_per_second_to_kilometers(stats['max_avg_speed']),
-                   self.remove_decimal_point(stats['average_cadence']),
-                   self.remove_decimal_point(stats['max_heartrate']),
+                  (activity_hyperlink % (stats['max_watts'], 'watts', stats['max_watts_activity']),
+                   activity_hyperlink % (stats['average_watts'], 'watts', stats['average_watts_activity']),
+                   activity_hyperlink % (self.meters_per_second_to_kilometers(stats['max_speed']), 'kmph', stats['max_speed_activity']),
+                   activity_hyperlink % (self.meters_per_second_to_kilometers(stats['max_avg_speed']), 'kmph', stats['max_avg_speed_activity']),
+                   activity_hyperlink % (self.remove_decimal_point(stats['average_cadence']), '', stats['average_cadence_activity']),
+                   activity_hyperlink % (self.remove_decimal_point(stats['max_heartrate']), 'bpm', stats['max_heartrate_activity']),
+                   activity_hyperlink % (self.meters_to_kilometers(stats['biggest_ride']), 'kms', stats['biggest_ride_activity']),
+                   activity_hyperlink % (self.remove_decimal_point(stats['max_elevation_gain']), 'meters', stats['max_elevation_gain_activity']),
                    stats['non_stop'],
-                   stats['biggest_ride'],
-                   stats['biggest_climb'],
                    self.seconds_to_human_readable(stats['break_time']),
                    stats['strava_created'],
                    stats['following'],
@@ -351,9 +370,9 @@ def get_config():
 
 def send_message(bot, update, message):
     bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-    update.message.reply_text(message, parse_mode="Markdown")
+    update.message.reply_text(message, parse_mode="Markdown", disable_web_page_preview=True)
     if shadow_mode:
-        bot.send_message(chat_id=shadow_chat_id, text=message, parse_mode="Markdown", disable_notification=True)
+        bot.send_message(chat_id=shadow_chat_id, text=message, parse_mode="Markdown", disable_notification=True, disable_web_page_preview=True)
 
 
 def get_athlete_token(bot, update):
@@ -437,4 +456,5 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     telegram_token, athletes, shadow_chat_id, admin_user_name, shadow_mode = get_config()
+    activity_hyperlink = """[%s %s](https://www.strava.com/activities/%s)"""
     main()
