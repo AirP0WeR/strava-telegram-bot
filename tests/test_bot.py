6eaa01ca7fe47ee22dfaaa000fe3f5b211059f2b
pancham <panchamrb@phonepe.com>
2018-04-24 21:15:45 +0530

    Add more tests

--- tests/test_bot.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from bot import InitializeBot
-
-class StravaTelegramBotTests(unittest.TestCase):
-
-    def test_strava_activity_hyperlink(self):
-        activity_hyperlink = InitializeBot().strava_activity_hyperlink()
-        self.assertEqual(activity_hyperlink, """[%s %s](https://www.strava.com/activities/%s)""")
