a9f809c30ac9534f5cd97db5a8ee9be13043e0e2
pancham <panchamrb@phonepe.com>
2018-05-24 22:40:20 +0530

    Fix import

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,5 +1,5 @@
+from client import StravaApi
 from .bot import Bot
-from .client.strava_api import StravaApi
 from .resources.fun_stats import FunStats
 from .resources.stats import Stats
 from .utils.aes_cipher import AESCipher
