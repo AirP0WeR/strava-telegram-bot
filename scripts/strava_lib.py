1b6b6bef41beafeb96fb23d2ee6e1c714e67e938
pancham <panchamrb@phonepe.com>
2018-07-13 11:00:57 +0530

    Rename method names

--- scripts/strava_lib.py
+++ scripts/strava_lib.py
@@ -8,20 +8,20 @@ class StravaLib(object):
     def __init__(self, athlete_token):
         logging.info("Initializing %s" % self.__class__.__name__)
         self.athlete_token = athlete_token
-        self.client = Client()
-        self.client.access_token = self.athlete_token
+        self.strava_client = Client()
+        self.strava_client.access_token = self.athlete_token
 
     def fetch_athlete(self):
-        return self.client.get_athlete()
+        return self.strava_client.get_athlete()
 
     def fetch_activities(self):
-        return self.client.get_activities()
+        return self.strava_client.get_activities()
 
     def fetch_starred_segments(self):
-        return self.client.get_starred_segment()
+        return self.strava_client.get_starred_segment()
 
     def fetch_segment_details(self, segment_id):
-        return self.client.get_segment(segment_id)
+        return self.strava_client.get_segment(segment_id)
 
     def fetch_segment_leader_board(self, segment_id):
-        return self.client.get_segment_leaderboard(segment_id)
+        return self.strava_client.get_segment_leaderboard(segment_id)
