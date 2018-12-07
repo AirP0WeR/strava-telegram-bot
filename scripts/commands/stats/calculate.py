441fa77af0424cf681437cbefb3d34a9480a5871
pancham <panchamrb@phonepe.com>
2018-12-07 15:02:08 +0530

    WIP: Ride Hundreds

--- scripts/commands/stats/calculate.py
+++ scripts/commands/stats/calculate.py
@@ -14,6 +14,7 @@ from scripts.commands.stats.ride_ytd import RideYtdStats
 from scripts.commands.stats.run_all_time import RunAllTimeStats
 from scripts.commands.stats.run_ytd import RunYtdStats
 from scripts.commands.stats.ride_misc import RideMiscStats
+from scripts.commands.stats.ride_hundreds import RideHundredsStats
 
 
 class CalculateStats(object):
@@ -35,11 +36,13 @@ class CalculateStats(object):
         ride_all_time_stats = RideAllTimeStats()
         ride_ytd_stats = RideYtdStats()
         ride_misc_stats = RideMiscStats()
+        ride_hundreds_stats = RideHundredsStats()
         run_all_time_stats = RunAllTimeStats()
         run_ytd_stats = RunYtdStats()
         input_ride_all_time_stats = ride_all_time_stats.input()
         input_ride_ytd_stats = ride_ytd_stats.input()
         input_ride_misc_stats = ride_misc_stats.input()
+        serial_no, input_list_ride_hundreds_stats = ride_hundreds_stats.input()
         input_run_all_time_stats = run_all_time_stats.input()
         input_run_ytd_stats = run_ytd_stats.input()
 
@@ -48,6 +51,9 @@ class CalculateStats(object):
                 input_ride_all_time_stats = ride_all_time_stats.calculate(input_ride_all_time_stats, activity)
                 input_ride_ytd_stats = ride_ytd_stats.calculate(input_ride_ytd_stats, activity, current_year)
                 input_ride_misc_stats = ride_misc_stats.calculate(input_ride_misc_stats, activity)
+                serial_no, input_list_ride_hundreds_stats = ride_hundreds_stats.calculate(serial_no,
+                                                                                          input_list_ride_hundreds_stats,
+                                                                                          activity)
             elif self.operations.is_activity_a_run(activity):
                 input_run_all_time_stats = run_all_time_stats.calculate(input_run_all_time_stats, activity)
                 input_run_ytd_stats = run_ytd_stats.calculate(input_run_ytd_stats, activity, current_year)
@@ -60,6 +66,8 @@ class CalculateStats(object):
         stats['all_time_run_stats'] = run_all_time_stats.format(input_run_all_time_stats)
         stats['ytd_run_stats'] = run_ytd_stats.format(input_run_ytd_stats)
         stats['misc_ride_stats'] = ride_misc_stats.format(input_ride_misc_stats)
+
+        print("100's: {}".format(input_list_ride_hundreds_stats))
 
         self.user_data['stats'] = stats
         self.update.message.reply_text(self.bot_constants.MESSAGE_STATS_MAIN_KEYBOARD_MENU,
---
+++ scripts/commands/stats/ride_hundreds.py
@@ -0,0 +1,44 @@
+#  -*- encoding: utf-8 -*-
+
+from os import sys, path
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from scripts.common.operations import Operations
+
+
+class RideHundredsStats(object):
+    MESSAGE_RIDE_HUNDREDS_STATS = "{serial_no}. [{activity_name}](https://www.strava.com/activities/{activity_id}) ({activity_date})"
+
+    def __init__(self):
+        self.operations = Operations()
+
+    # @staticmethod
+    # def input():
+    #     return {
+    #         'count': 0,
+    #         'activity_name': '',
+    #         'activity_id': '',
+    #         'activity_date': ''
+    #     }
+
+    @staticmethod
+    def input():
+        serial_no = 0
+        list_ride_hundreds_stats = list()
+        return serial_no, list_ride_hundreds_stats
+
+    def calculate(self, serial_no, input_list_ride_hundreds_stats, activity):
+        if float(activity.distance) >= 100000:
+            # input_ride_hundreds_stats['count'] += 1
+            # input_ride_hundreds_stats['activity_name'] = activity.name
+            # input_ride_hundreds_stats['activity_id'] = activity.id
+            # input_ride_hundreds_stats['activity_date'] = activity.start_date_local.date()
+            serial_no += 1
+            input_list_ride_hundreds_stats.append(self.MESSAGE_RIDE_HUNDREDS_STATS.format(
+                serial_no=serial_no,
+                activity_name=activity.name,
+                activity_id=activity.id,
+                activity_date=activity.start_date_local.date()
+            ))
+
+        return input_list_ride_hundreds_stats
