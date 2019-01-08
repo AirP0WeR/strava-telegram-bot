8405cc7c5f207e95460e540368f2834b2797655d
pancham <panchamrb@phonepe.com>
2019-01-08 14:20:08 +0530

    Remove aes cipher

--- scripts/handle/commands.py
+++ scripts/handle/commands.py
@@ -10,7 +10,6 @@ import requests
 import telegram
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from scripts.common.aes_cipher import AESCipher
 from scripts.common.constants_and_variables import BotVariables, BotConstants
 from scripts.commands.stats.calculate import CalculateStats
 
@@ -23,20 +22,19 @@ class HandleCommands(object):
         self.user_data = user_data
         self.bot_variables = BotVariables()
         self.bot_constants = BotConstants()
-        self.aes_cipher = AESCipher(self.bot_variables.crypt_key_length, self.bot_variables.crypt_key)
         self.athlete_token = None
 
     def refresh_and_update_token(self, athlete_id, refresh_token):
         response = requests.post(self.bot_constants.API_TOKEN_EXCHANGE, data={
-            'client_id': int(self.aes_cipher.decrypt(self.bot_variables.client_id)),
-            'client_secret': self.aes_cipher.decrypt(self.bot_variables.client_secret),
+            'client_id': int(self.bot_variables.client_id),
+            'client_secret': self.bot_variables.client_secret,
             'grant_type': 'refresh_token',
             'refresh_token': refresh_token,
         }).json()
 
         access_info = dict()
-        access_info['access_token'] = self.aes_cipher.encrypt(response['access_token'])
-        access_info['refresh_token'] = self.aes_cipher.encrypt(response['refresh_token'])
+        access_info['access_token'] = response['access_token']
+        access_info['refresh_token'] = response['refresh_token']
         access_info['expires_at'] = response['expires_at']
 
         database_connection = psycopg2.connect(self.bot_variables.database_url, sslmode='require')
@@ -62,8 +60,8 @@ class HandleCommands(object):
         database_connection.close()
         if result:
             athlete_id = result[0][0]
-            access_token = self.aes_cipher.decrypt(result[0][1])
-            refresh_token = self.aes_cipher.decrypt(result[0][2])
+            access_token = result[0][1]
+            refresh_token = result[0][2]
             expires_at = result[0][3]
             current_time = int(time.time())
             if current_time > expires_at:
@@ -111,5 +109,5 @@ class HandleCommands(object):
             message = self.bot_constants.MESSAGE_UNREGISTERED_ATHLETE.format(
                 first_name=self.update.message.from_user.first_name,
                 registration_url=self.bot_variables.registration_url,
-                admin_user_name=self.aes_cipher.decrypt(self.bot_variables.admin_user_name))
+                admin_user_name=self.bot_variables.admin_user_name)
             self.update.message.reply_text(message, parse_mode="Markdown", disable_web_page_preview=True)
