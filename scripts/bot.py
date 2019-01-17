b12f548444d5787cb651f1778aa847610b849294
pancham <panchamrb@phonepe.com>
2019-01-17 10:24:33 +0530

    Exception handling

--- scripts/bot.py
+++ scripts/bot.py
@@ -21,15 +21,23 @@ class StravaTelegramBot(object):
     def error(update, error):
         logger.error('Update "{update}" caused error "{error}"'.format(update=update, error=error))
 
-    @staticmethod
-    def handle_commands(bot, update, user_data):
-        commands = HandleCommands(bot, update, user_data)
-        commands.process()
+    def handle_commands(self, bot, update, user_data):
+        try:
+            commands = HandleCommands(bot, update, user_data)
+            commands.process()
+        except Exception:
+            message = "Something went wrong. Exception: {exception}".format(exception=traceback.format_exc())
+            logging.error(message)
+            self.shadow_mode.send_message(message)
 
-    @staticmethod
-    def handle_buttons(bot, update, user_data):
-        buttons = HandleButtons(bot, update, user_data)
-        buttons.process()
+    def handle_buttons(self, bot, update, user_data):
+        try:
+            buttons = HandleButtons(bot, update, user_data)
+            buttons.process()
+        except Exception:
+            message = "Something went wrong. Exception: {exception}".format(exception=traceback.format_exc())
+            logging.error(message)
+            self.shadow_mode.send_message(message)
 
     def main(self):
         try:
--- scripts/handle/buttons.py
+++ scripts/handle/buttons.py
@@ -1,11 +1,7 @@
 #  -*- encoding: utf-8 -*-
 
-import logging
-import traceback
-
 from buttons.auto_update_indoor_ride import AutoUpdateIndoorRide
 from buttons.stats import Stats
-from common.shadow_mode import ShadowMode
 
 
 class HandleButtons(object):
@@ -16,19 +12,12 @@ class HandleButtons(object):
         self.user_data = user_data
         self.query = self.update.callback_query
         self.chosen_option = self.query.data
-        self.shadow_mode = ShadowMode()
 
     def process(self):
-        try:
-            if self.chosen_option.startswith('stats'):
-                stats = Stats(self.bot, self.update, self.user_data)
-                stats.process()
+        if self.chosen_option.startswith('stats'):
+            stats = Stats(self.bot, self.update, self.user_data)
+            stats.process()
 
-            elif self.chosen_option.startswith('auto_update_indoor_ride'):
-                setup = AutoUpdateIndoorRide(self.bot, self.update, self.user_data, self.chosen_option)
-                setup.process()
-
-        except Exception:
-            message = "Something went wrong. Exception: {exception}".format(exception=traceback.format_exc())
-            logging.error(message)
-            self.shadow_mode.send_message(message)
+        elif self.chosen_option.startswith('auto_update_indoor_ride'):
+            setup = AutoUpdateIndoorRide(self.bot, self.update, self.user_data, self.chosen_option)
+            setup.process()
