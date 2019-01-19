de176468834aaa469c7312c09ab097204276f63c
pancham <panchamrb@phonepe.com>
2019-01-19 10:57:33 +0530

    Handle TimedOut exception

--- scripts/common/shadow_mode.py
+++ scripts/common/shadow_mode.py
@@ -3,6 +3,8 @@
 import logging
 import traceback
 
+import requests
+from telegram.error import TimedOut
 from telegram.ext.dispatcher import run_async
 
 from common.constants_and_variables import BotConstants, BotVariables
@@ -23,5 +25,19 @@ class ShadowMode(object):
                 self.bot.send_message(chat_id=self.bot_variables.shadow_mode_chat_id, text=message,
                                       parse_mode=parse_mode, disable_web_page_preview=disable_web_page_preview,
                                       disable_notification=disable_notification, reply_markup=reply_markup)
+        except TimedOut():
+            logging.warning("Telegram Timed Out. Retrying with API...".format(exception=traceback.format_exc()))
+            data = {
+                'chat_id': '{chat_id}'.format(chat_id=self.bot_variables.shadow_mode_chat_id),
+                'text': '{message}'.format(message=message),
+                'parse_mode': parse_mode,
+                'disable_web_page_preview': disable_web_page_preview,
+                'disable_notification': disable_notification,
+                'reply_markup': reply_markup
+            }
+            requests.post(
+                self.bot_constants.API_TELEGRAM_SEND_MESSAGE.format(bot_token=self.bot_variables.telegram_bot_token,
+                                                                    data=data))
+
         except Exception:
             logging.error("Something went wrong. Exception: {exception}".format(exception=traceback.format_exc()))
