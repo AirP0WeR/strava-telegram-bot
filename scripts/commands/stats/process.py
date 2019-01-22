e310f5d6824090391f23bb006d792e2dc8ea691e
pancham <panchamrb@phonepe.com>
2019-01-22 14:15:32 +0530

    Fix arguments

--- scripts/commands/stats/process.py
+++ scripts/commands/stats/process.py
@@ -15,4 +15,4 @@ class ProcessStats(object):
     def process(self):
         message = self.bot_constants.MESSAGE_STATS_MAIN_KEYBOARD_MENU
         self.update.message.reply_text(message, reply_markup=self.bot_constants.KEYBOARD_STATS_MAIN_KEYBOARD_MENU)
-        self.shadow_mode.send_message(bot=self.bot, message=message)
+        self.shadow_mode.send_message(message=message)
